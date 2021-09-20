2a2fcd2ca97443c4454225600e1c3a551cb0386d
Berend Sliedrecht <berend@animo.id>
2021-09-20 12:05:36 +0200

    feat: initialize cli
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+/// Initializes the application
 fn main() {
-    println!("Hello, world!");
+    // Load the yaml file containing the cli setup
+    let yaml = load_yaml!("../cli.yaml");
+
+    // Get all the supplied flags and values
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Load the configuration file
+    let _config = matches.value_of("config").unwrap_or("default.conf");
 }
