2c863d673cfb42f777b19ce5bb4de23be2c1ebfe
Berend Sliedrecht <berend@animo.id>
2021-12-12 13:07:19 +0100

    fix: abstracted pretty print
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- src/cli/connections.rs
+++ src/cli/connections.rs
@@ -1,31 +1,18 @@
 use crate::agent::agents::Agent;
 use crate::typing::ConnectionsConfig;
 use crate::utils::logger::Log;
-use serde::Serialize;
 
 pub async fn run(agent: &dyn Agent, config: ConnectionsConfig<'_>) {
     match config.id {
         Some(i) => {
             let connection = agent.get_connection_by_id(i).await;
 
-            let buf = Vec::new();
-            let formatter = serde_json::ser::PrettyFormatter::with_indent(b"  ");
-            let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
-
-            connection.serialize(&mut ser).unwrap();
-
-            Log::log(&String::from_utf8(ser.into_inner()).unwrap());
+            Log::pretty_log(&connection);
         }
         None => {
             let connections = agent.get_connections(config.alias).await.results;
 
-            let buf = Vec::new();
-            let formatter = serde_json::ser::PrettyFormatter::with_indent(b"  ");
-            let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
-
-            connections.serialize(&mut ser).unwrap();
-
-            Log::log(&String::from_utf8(ser.into_inner()).unwrap());
+            Log::pretty_log(&connections);
         }
     };
 }
--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,8 +1,19 @@
 use colored::*;
+use serde::Serialize;
 
 pub struct Log;
 
 impl Log {
+    pub fn pretty_log<T: Serialize>(obj: &T) {
+        let buf = Vec::new();
+        let formatter = serde_json::ser::PrettyFormatter::with_indent(b"  ");
+        let mut ser = serde_json::Serializer::with_formatter(buf, formatter);
+
+        obj.serialize(&mut ser).unwrap();
+
+        Log::log(&String::from_utf8(ser.into_inner()).unwrap());
+    }
+
     // Log messages that contain generic info
     pub fn log(string: &str) {
         println!("{}", String::from(string));
