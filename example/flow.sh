3c15f49066a4a494843b41178fd9509d1767c8dd
Berend Sliedrecht <berend@animo.id>
2022-02-06 17:27:41 +0100

    cleanup example
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- example/flow.sh
+++ example/flow.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
-acl issue-credential --full-output --connection-id $(acl connections | jq -r '.[0].connection_id') --credential-definition-id $(acl credential-definition | jq -r '.[0]') --key leeftijd --value 10 --key naam --value bob
+# Issue credential
+acl issue-credential --full-output \ 
+                     --connection-id $(acl connections | jq -r '.[0].connection_id') \
+                     --credential-definition-id $(acl credential-definition | jq -r '.[0]') \
+                     --key leeftijd \
+                     --value 10 \
+                     --key naam \
+                     --value bob
