19ae4a28af66017c7e2179c58e6d275b8e3d48a9
Berend Sliedrecht <berend@animo.id>
2022-03-14 11:07:00 +0100

    fix: tests work
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- tests/run.sh
+++ tests/run.sh
@@ -38,7 +38,6 @@ schemas() {
   cargo run -q -- -e=$ENDPOINT schemas &> /dev/null
   handle_out $? 0 "Schemas: Get All"
 
-  echo $SCHEMA_ID
   cargo run -q -- -e=$ENDPOINT schemas --id=$SCHEMA_ID &> /dev/null
   handle_out $? 0 "Schemas: Get By Id"
 }
@@ -47,20 +46,20 @@ credential_definitions() {
   echo "--- Credential Definitions --- "
   SCHEMA_ID=`cargo run -q -- -e=$ENDPOINT schemas create -n=foo -a=bar -a=baz 2> /dev/null`
 
-  CRED_DEF_ID=`cargo run -q -- -q -e=$ENDPOINT  credential-definitions create --schema-id=$SCHEMA_ID`
+  CRED_DEF_ID=`cargo run -q -- -e=$ENDPOINT credential-definitions create --schema-id=$SCHEMA_ID 2> /dev/null`
   handle_out $? 0 "Credential Definitions: create"
 
   cargo run -q -- -q -e=$ENDPOINT  credential-definitions &> /dev/null
-  handle_out $? 0 "Credential Definition: Get All"
+  handle_out $? 0 "Credential Definitions: Get All"
 
-  cargo run -q -- -q -e=$ENDPOINT  credential-definitions --id=$CRED_DEF_ID &> /dev/null  
-  handle_out $? 0 "Credential Definition: Get By Id"
+  cargo run -q -- -q -e=$ENDPOINT  credential-definitions --id=$CRED_DEF_ID &> /dev/null
+  handle_out $? 0 "Credential Definitions: Get By Id"
 }
 
 credentials() {
   echo "--- Credentials ----"
-  SCHEMA_ID=`cargo run -q -- -e=$ENDPOINT schemas create -n=foo -a=bar -a=baz 2> /dev/null`
-  CRED_DEF_ID=`cargo run -q -- -q -e=$ENDPOINT  credential-definitions create --schema-id=$SCHEMA_ID`
+  SCHEMA_ID=`cargo run -q -- -e=$ENDPOINT schemas create -n=foo -a=bar -a=baz 2> /dev/null 2> /dev/null`
+  CRED_DEF_ID=`cargo run -q -- -e=$ENDPOINT  credential-definitions create --schema-id=$SCHEMA_ID 2> /dev/null`
   cargo run -q -- -q credentials offer --connection-id=FOO --cred-def-id=$CRED_DEF_ID -k=bar -v=B -k=baz -v=C &> /dev/null
   handle_out $? 1 "credentials: Offer"
 }
@@ -72,10 +71,10 @@ handle_out() {
 
   if [[ $EXIT_CODE != $SHOULD_BE ]]
   then
-    echo "test: ${TEST_NAME} went wrong." 
+    echo "${TEST_NAME} went wrong."
     exit 1
   else
-    echo "test: ${TEST_NAME} succeeded!"
+    echo "${TEST_NAME} finished correctly with status $1"
   fi
 }
 
@@ -83,6 +82,6 @@ handle_out() {
 features
 message
 connections
-#schemas
-#credential_definitions
-#credentials
+schemas
+credential_definitions
+credentials
