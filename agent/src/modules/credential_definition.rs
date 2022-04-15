8568e985264b9216a95aed8be41a0d4c22801410
blu3beri <berend@animo.id>
2022-04-15 11:44:58 +0200

    feat: added support for revocation and tags in credential definition
    Signed-off-by: blu3beri <berend@animo.id>

--- agent/src/modules/credential_definition.rs
+++ agent/src/modules/credential_definition.rs
@@ -3,6 +3,16 @@ use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+#[derive(Debug, Deserialize, Serialize, Default)]
+pub struct CredentialDefinitionCreateOptions {
+    pub schema_id: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub tag: Option<String>,
+    pub support_revocation: bool,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub revocation_registry_size: Option<i32>,
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct CredentialDefinitionCreateResponse {
     pub credential_definition_id: String,
@@ -35,7 +45,10 @@ pub struct CredentialDefinitionGetAllResponse {
 #[async_trait]
 pub trait CredentialDefinitionModule {
     /// Requests all the features from the cloudagent
-    async fn create(&self, schema_id: String) -> Result<CredentialDefinitionCreateResponse>;
+    async fn create(
+        &self,
+        options: CredentialDefinitionCreateOptions,
+    ) -> Result<CredentialDefinitionCreateResponse>;
     async fn get_by_id(&self, id: String) -> Result<CredentialDefinitionGetByIdResponse>;
     async fn get_all(&self) -> Result<CredentialDefinitionGetAllResponse>;
 }
