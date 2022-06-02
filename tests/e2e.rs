9cb5c971cf6983609206a38dcfd528d5c196a891
Jean-Louis Leysens <jloleysens@gmail.com>
2022-06-02 13:31:21 +0200

    First iteration of e2e tests (#183)
    * e2e tests wip
    
    * more wip
    
    * first version of a few e2e tests
    
    * slight refactor to tidy up tests from helper logic
    
    * rename tests
    
    * added readme
    
    * added a message to assertion, use json! macro and regroup uses

---
+++ tests/e2e.rs
@@ -0,0 +1,46 @@
+#![cfg(test)]
+
+use agent::modules::connection::Connection;
+use regex::Regex;
+use speculoos::prelude::*;
+mod helpers;
+use helpers::run_test;
+
+#[tokio::test]
+async fn smoke() -> () {
+    run_test(|agent_cli| {
+        let re = Regex::new(r"^agent-cli \d\.\d.\d").unwrap();
+        assert_that(&agent_cli.exec(&["--version"])).matches(|v| re.is_match(v))
+    })
+    .await
+}
+
+#[tokio::test]
+async fn create_a_connection_and_list_connections() -> () {
+    run_test(|agent_cli| {
+        let connections = agent_cli.exec(&["connection", "list"]);
+        assert_that(&connections).is_equal_to(&String::from("[]"));
+        let _ = agent_cli.exec(&["connection", "invite", "--alias", "test"]);
+        let connections_str = agent_cli.exec(&["connection", "list"]);
+        let connections: Vec<Connection> = serde_json::from_str(&connections_str).unwrap();
+        assert_that(&connections).has_length(1);
+    })
+    .await;
+}
+
+#[tokio::test]
+async fn create_a_connection_and_send_a_message() -> () {
+    run_test(|agent_cli| {
+        let _ = agent_cli.exec(&["connection", "invite", "--alias", "foo"]);
+        let connections_str = agent_cli.exec(&["connection", "list"]);
+        let connection: Vec<Connection> = serde_json::from_str(&connections_str).unwrap();
+        let result = agent_cli.exec(&[
+            "message",
+            "--connection-id",
+            &connection[0].connection_id,
+            "--message=BAR",
+        ]);
+        assert_that(&result).is_equal_to(String::from(""))
+    })
+    .await
+}
