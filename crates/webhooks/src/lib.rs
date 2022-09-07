ccaa43aeb71c94b091f3ad0eef570f8a9264a692
Moriarty <moritz@animo.id>
2022-09-07 11:32:36 +0200

    feat: some improvement to parse url conditionally and pretty print
    Signed-off-by: Moriarty <moritz@animo.id>

--- crates/webhooks/src/lib.rs
+++ crates/webhooks/src/lib.rs
@@ -1,9 +1,12 @@
 use tungstenite::{connect};
 use url::Url;
-use serde_json;
+use serde_json::{from_str, to_string_pretty};
+use colored::Colorize;
 
 
-pub fn listen(url: &Option<String>) {
+pub fn listen(url: &Option<String>) -> ! {
+    // TODO: filter by/listen to by topic
+    // TODO: Dont parse the url from args but derive it from the agent url in config
     let listen_url = 
         match url.as_deref() {
             Some(u) => u,
@@ -12,14 +15,17 @@ pub fn listen(url: &Option<String>) {
     
     let (mut socket, _response) = connect(Url::parse(listen_url).unwrap()).expect("Can't connect to websocket");    // Write a message containing "Hello, Test!" to the server
     
+    // TODO: Pretty print and potentially transform to some relevant only fields output
     // Loop forever, handling parsing each message
     loop {
+        // TODO: Replace with error string from enum
         let msg = socket.read_message().expect("Error reading message");
         let msg = match msg {
             tungstenite::Message::Text(s) => { s }
             _ => { panic!() }
         };
-        let parsed: serde_json::Value = serde_json::from_str(&msg).expect("Can't parse to JSON");
-        println!("Received hook:\n{:?}", parsed);
+        // TODO: Replace with error string from enum
+        let parsed: serde_json::Value = from_str(&msg).expect("Can't parse to JSON");
+        println!("{}{}", format!("Received hook:\n").yellow(), to_string_pretty(&parsed).unwrap());
     }
 }
