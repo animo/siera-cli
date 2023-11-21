d32e8229c03146c98dbe696df5e4b9d97e06c1b0
Berend Sliedrecht <61358536+berendsliedrecht@users.noreply.github.com>
2023-11-21 12:10:21 +0100

    use secure websocket when https is used (#273)
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- crates/cloudagent-python/src/cloudagent/webhook.rs
+++ crates/cloudagent-python/src/cloudagent/webhook.rs
@@ -8,20 +8,22 @@ use tungstenite::connect;
 impl WebhookModule for CloudAgentPython {
     /// Listen to all incoming webhook
     async fn listen(&self, on_event: fn(serde_json::Value)) -> Result<()> {
-        let stripped_agent_url = match &self.endpoint {
-            s if s.starts_with("http://") => &s[7..],
-            s if s.starts_with("https://") => &s[8..],
+        let (uses_tls, stripped_agent_url) = match &self.endpoint {
+            s if s.starts_with("http://") => (false, &s[7..]),
+            s if s.starts_with("https://") => (true, &s[8..]),
             s => return Err(Error::InvalidAgentUrl(s.clone()).into()),
         };
 
-        let listen_url = format!("wss://{stripped_agent_url}/ws");
+        let scheme = if uses_tls { "wss" } else { "ws" };
+
+        let listen_url = format!("{scheme}://{stripped_agent_url}/ws");
         info!({ "message": format!("Listening on {listen_url}") });
 
         let (mut socket, _response) = connect(listen_url)?;
 
         // Loop forever, parse message to stdout
         loop {
-            let message = socket.read_message()?;
+            let message = socket.read()?;
             let parsed: serde_json::Value = serde_json::from_str(&message.to_string())?;
             on_event(parsed);
         }
